2c94e92fe8f609d3f49bdc65f716a374d898f1d8
11tuvork28 <zofi@xamh.de>
2021-09-15 17:23:12 +0200

    Added contact form functionality

--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
-from web.models import Fotos, WilkommensText
-from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.shortcuts import redirect, render
 from .models import *
+from .forms import ContactForm
 
 def index(request):
     context = {
@@ -9,9 +10,22 @@ def index(request):
         "blogs": Blog.objects.all().order_by('-id')[:3],
         "sponsoren":  Sponsoren.objects.all(),
         "roadmap": Roadmap.objects.all(), 
-        "kontakt": Kontakt.objects.filter(Aktuell=True)
+        "kontakt": Kontakt.objects.filter(Aktuell=True),
+        "form": ContactForm(),
 
     }
+    if request.method != 'GET':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['zofi@xamh.de', from_email])
+            except BadHeaderError:
+                pass
+            redirect('index')
+
     return render(request=request, template_name="web/home.html", context=context)
 
  
